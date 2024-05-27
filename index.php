6613cdfb4c131c5046fd7ff1ad29caf15a5ea098
arrays2683 <anthon2712@gmail.com>
2024-05-27 04:28:09 -0300

    Add Logo

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-        <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="it28-admin\images\Final Logo.png" width="50" height="50" class="d-inline-block align-top" alt="">
         PARANE FOOD HUB
     </a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
