3015bd7d38db29e6b6c4a7dd7b003004a7e0eb10
arrays2683 <anthon2712@gmail.com>
2024-06-02 09:34:08 -0300

    changed the config into live hosting

--- it28-admin/address/config.php
+++ it28-admin/address/config.php
@@ -1,16 +1,16 @@
 <?php
 // config.php
 
-$host = 'localhost';
-$dbname = 'parane1';
-$username = 'root';
-$password = '';
-/*
+// $host = 'localhost';
+// $dbname = 'parane1';
+// $username = 'root';
+// $password = '';
+
 $host = 'localhost';
 $dbname = 'u593341949_db_parane';
 $username = 'u593341949_dev_parane';
 $password = '20212088Parane';
-*/
+
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
 try {
