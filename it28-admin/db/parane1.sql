393e6133f8b1005bf181d4de549c64a99c1c0c78
arrays2683 <anthon2712@gmail.com>
2024-06-02 09:16:05 -0300

    updated the sql dump

--- it28-admin/db/parane1.sql
+++ it28-admin/db/parane1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2024 at 01:38 AM
+-- Generation Time: Jun 02, 2024 at 02:14 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -43,12 +43,9 @@ CREATE TABLE `addresses` (
 --
 
 INSERT INTO `addresses` (`address_id`, `street`, `city`, `state`, `zip`, `country`, `created_at`, `payment_id`) VALUES
-(1, 'kalanawa', 'bukidnon', 'mindanao', '3232', 'Philippines', '2024-05-23 09:47:34', 0),
-(2, 'Calanawan', 'Manolo Fortich Bukidnon', 'Mindanao', '8703', 'Philippines', '2024-05-24 02:23:42', 0),
-(3, 'kalanawa', 'bukidnon', 'mindanao', '3232', 'Philippines', '2024-05-27 09:14:23', 0),
-(4, 'kalanawa', 'bukidnon', 'mindanao', '3232', 'Philippines', '2024-05-27 09:34:12', 0),
-(5, 'Calanawan', 'Manolo Fortich, Bukidnon', 'Mindanao', '3232', 'Philippines', '2024-05-29 22:51:55', 0),
-(6, 'Calanawan', 'Manolo Fortich, Bukidnon', 'Mindanao', '3232', 'Philippines', '2024-05-29 23:12:08', 0);
+(19, 'Calanawan', 'Manolo Fortich, Bukidnon', 'Mindanao', '3232', 'Philippines', '2024-06-02 12:01:44', 0),
+(20, 'Calanawan', 'Manolo Fortich, Bukidnon', 'Mindanao', '3232', 'Philippines', '2024-06-02 12:08:31', 0),
+(21, 'sample', 'sample', 'sample', 'sample', 'sample', '2024-06-02 12:10:32', 0);
 
 -- --------------------------------------------------------
 
@@ -71,14 +68,8 @@ CREATE TABLE `payments` (
 --
 
 INSERT INTO `payments` (`payment_id`, `product_name`, `price`, `total`, `payment_method`, `created_at`, `product_id`) VALUES
-(3, 'Taho', 30.00, 0.00, 'PayPal', '2024-05-23 09:26:10', 0),
-(5, 'Fish Balls', 18.00, 0.00, 'PayMaya', '2024-05-23 09:27:36', 0),
-(6, 'Balut', 20.00, 0.00, 'GCash', '2024-05-23 09:28:55', 0),
-(7, 'Balut', 20.00, 0.00, 'PayPal', '2024-05-24 02:23:09', 0),
-(8, 'Isaw', 25.00, 0.00, 'PayPal', '2024-05-27 09:14:19', 0),
-(9, 'Balut', 20.00, 0.00, 'PayPal', '2024-05-27 09:34:06', 0),
-(10, 'Balut', 20.00, 0.00, 'PayPal', '2024-05-29 22:51:49', 0),
-(11, 'Fish Balls', 18.00, 0.00, 'PayPal', '2024-05-29 23:11:56', 0);
+(16, 'Isaw', 25.00, 25.00, 'GCash', '2024-06-02 11:29:27', 1),
+(33, 'Isaw', 25.00, 175.00, 'GCash', '2024-06-02 12:08:26', 1);
 
 -- --------------------------------------------------------
 
@@ -88,7 +79,7 @@ INSERT INTO `payments` (`payment_id`, `product_name`, `price`, `total`, `payment
 
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL,
-  `title` varchar(200) NOT NULL,
+  `product_name` varchar(200) NOT NULL,
   `description` text NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `rrp` decimal(10,0) NOT NULL DEFAULT 0,
@@ -102,7 +93,7 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `title`, `description`, `price`, `rrp`, `quantity`, `img`, `date_added`, `user_id`) VALUES
+INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `rrp`, `quantity`, `img`, `date_added`, `user_id`) VALUES
 (1, 'Isaw', 'Grilled chicken or pork intestines marinated in a tangy and spicy sauce.', 20, 25, 50, 'https://www.lasabbq.com/cdn/shop/products/BBQChickenIsaw1.jpg', '2024-05-08 00:00:00', 0),
 (2, 'Balut', 'Fertilized duck embryo boiled and commonly sold as street food in the Philippines.', 15, 20, 30, 'https://facts.net/wp-content/uploads/2020/10/AdobeStock_279704615.jpeg', '2024-05-08 00:00:00', 0),
 (3, 'Kwek-Kwek', 'Quail eggs coated in orange batter and deep-fried, often served with vinegar.', 10, 15, 40, 'https://www.kawalingpinoy.com/wp-content/uploads/2019/07/kwek-kwek-14.jpg', '2024-05-08 00:00:00', 0),
@@ -128,7 +119,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `created_at`) VALUES
 (1, 'admin', '$2y$10$kGp4g1TjBK4XwLIwRbBHSeZ4W5FpPbYoB1ap5NfFUjUPAcE3KR5QG', '2024-04-29 16:39:58'),
-(2, 'arlene', '$2y$10$JbxintLe9rHvRuCGIVZfd.fk943ESa5pITzzXJ6.4bTBsdUj3CqJa', '2024-05-23 23:22:35');
+(2, 'arlene', '$2y$10$JbxintLe9rHvRuCGIVZfd.fk943ESa5pITzzXJ6.4bTBsdUj3CqJa', '2024-05-23 23:22:35'),
+(3, 'test1', '$2y$10$.RkUpNRmmXtp0ZGe97ojG.lZjZq8nBuTl60TsXiwq1rVkC/lV/7Xe', '2024-05-30 04:17:31'),
+(4, 'temp', '$2y$10$ptpvm642.ZqY9BZ48qrYy.vO4dsNXmTNN8zXIOus1aSHV8JxyQlGW', '2024-05-30 04:17:50');
 
 --
 -- Indexes for dumped tables
@@ -166,25 +159,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `address_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `address_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
